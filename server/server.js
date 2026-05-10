5b566785660da7c7c291f780b1845e664e372701
Aiman Nishat <aimannishat78692@gmail.com>
2026-05-10 21:36:36 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-
 import authRoutes from "./routes/auth.js";
 import employeeRoutes from "./routes/employeeRoutes.js";
 import attendanceRoutes from "./routes/attendanceRoutes.js";
@@ -10,29 +9,24 @@ import leaveRoutes from "./routes/leaveRoutes.js";
 import dashboardRoutes from "./routes/dashboardRoutes.js";
 
 dotenv.config();
-
 const app = express();
+
 app.use(cors({
   origin: "*",
   credentials: false
 }));
 app.use(express.json());
+
 connectDB();
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/employees", employeeRoutes);
 app.use("/api/attendance", attendanceRoutes);
 app.use("/api/leaves", leaveRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 
-
 app.get("/", (req, res) => {
   res.send("HRMS Backend Running...");
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server Running on Port ${PORT}`);
-});
+export default app;
