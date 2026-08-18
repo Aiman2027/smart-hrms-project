8d6b14eb0370de0db36cbff718ce61cd50782efe
Aiman Nishat <aimannishat78692@gmail.com>
2026-08-18 18:00:14 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,16 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined");
+    }
+
     await mongoose.connect(process.env.MONGO_URI);
+
     console.log("MongoDB Connected");
   } catch (error) {
-    console.log("DB Error:", error.message);
-    process.exit(1);
+    console.error("DB Error:", error.message);
+    throw error;
   }
 };
 
