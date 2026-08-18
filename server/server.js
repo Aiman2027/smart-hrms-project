1572c109d4fbb84195b9740cb69ff0c94aa6575f
Aiman Nishat <aimannishat78692@gmail.com>
2026-08-18 17:45:30 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+
 import authRoutes from "./routes/auth.js";
 import employeeRoutes from "./routes/employeeRoutes.js";
 import attendanceRoutes from "./routes/attendanceRoutes.js";
@@ -9,15 +10,43 @@ import leaveRoutes from "./routes/leaveRoutes.js";
 import dashboardRoutes from "./routes/dashboardRoutes.js";
 
 dotenv.config();
+
 const app = express();
 
-app.use(cors({
-  origin: "*",
-  credentials: false
-}));
+const allowedOrigins = [
+  "https://smart-hrms-project-b9lx.vercel.app",
+  "http://localhost:5173",
+  "http://localhost:3000"
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+     
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
+
+
+app.options("*", cors());
+
 app.use(express.json());
 
+
 connectDB();
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/employees", employeeRoutes);
